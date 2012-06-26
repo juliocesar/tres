93b0f162c7f60a7f06656c7174c99dc6c756e238
Julio Cesar Ody <julioody@gmail.com>
2012-06-27 00:06:30 +1000

    Initial obvious stuff for Tres.Screen

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
+  window.JST = {
+    'screen' : $('#screen-template').html()
+  };
+  
   window.App = {};
   
   App.Device = new Tres.Device;
+  App.Device.on('orientationchange', function() {
+    alert("width : " + this.width() + ' - height : ' + this.height());
+  });
   
+  App.HomeScreen = new Tres.Screen({ template : JST['screen'] });
+  
+  App.HomeScreen.embed();
 });
--- tres.js
+++ tres.js
@@ -2,7 +2,9 @@
   var Tres = {},
     $ = window.$,
     _ = window._,
+    JST = window.JST,
     $window = $(window),
+    $body = $('body'),
     Backbone = window.Backbone;
 
   var Device = function() {
@@ -14,8 +16,32 @@
     
     $window.on('orientationchange', _.bind(me.trigger, me, 'orientationchange'));
   };
-  
   Tres.Device = Device;
+  
+  var Screen = Backbone.View.extend({
+    tagName : 'section',
+    
+    initialize : function(options) {
+      if (_.isUndefined(options)) options = {};
+      _.extend(this, options);
+    },
+    
+    render : function() {
+      this.$el.html( this.template );
+      this.rendered = true;
+      return this;
+    },
+    
+    embed : function() {
+      if (!this.rendered) this.render();      
+      $body.append(this.el);
+    },
+    
+    back : function(event) {
+      history.back();
+    }
+  });
+  Tres.Screen = Screen;
 
   window.Tres = Tres;
 })(this);

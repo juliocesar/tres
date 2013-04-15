b4e8736f227530ba1b0aba43081e713ab0adba9b
Julio Cesar Ody <julio@awesomebydesign.com>
2013-04-15 11:33:10 +1000

    Fix reference to current directories in packager

--- lib/tres/packager.rb
+++ lib/tres/packager.rb
@@ -5,7 +5,7 @@ module Tres
     include FileMethods
 
     attr_accessor :root, :asset_manager
-    
+
     def initialize options = {}
       @root = options[:root]
       @asset_manager = options[:asset_manager]
@@ -19,11 +19,11 @@ module Tres
       index = Nokogiri::HTML(read_file(@root/'build'/'index.html'))
       scripts = index.css('script[src]')
       scripts.each do |script|
-        write relativize(script['src'], '/.')
+        write relativize(script['src'], '.')
       end
       styles = index.css('link[rel="stylesheet"][href]')
       styles.each do |style|
-        write relativize(style['href'], '/.') rescue nil
+        write relativize(style['href'], '.') rescue nil
       end
     end
   end
